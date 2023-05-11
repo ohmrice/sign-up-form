087284d7ef03b78377c9717e5b5f430226c0cd41
ΩRICE <33384459+ohmrice@users.noreply.github.com>
2023-05-11 17:26:52 +0800

    Add password confirmation

---
+++ js-file.js
@@ -0,0 +1,42 @@
+const inputs = document.querySelectorAll(".input");
+const confirmMsg  = document.querySelector(".confirm-message");
+const submitBtn = document.querySelector("#submit-button");
+
+submitBtn.addEventListener("click", (event) => {
+    if (passwordsMatch) {
+
+        return true;
+    } else {
+        console.log("No");
+        event.preventDefault();
+    }
+});
+
+inputs.forEach((textField) => textField.addEventListener("focus", () => {
+    textField.classList.add("invalid-input");
+    if (textField.name === "confirm-password" || textField.name === "password") {
+        let password1 = document.querySelector("#password");
+        let password2 = document.querySelector("#confirm-password");
+        textField.addEventListener("input", () => confirmPassword(password1, password2));
+    }
+}));
+
+let passwordsMatch = false;
+
+function confirmPassword (pw1, pw2) {   
+        if (pw1.value === pw2.value) {
+            passwordsMatch = true;
+            console.log(passwordsMatch);
+            pw2.style.backgroundColor = "rgb(238, 234, 216)";
+            pw2.style.borderColor = "rgb(238, 234, 216)";
+            confirmMsg.textContent = "Passwords match";
+            confirmMsg.style.color = "rgb(126, 148, 98)";
+            return;
+        }
+        confirmMsg.textContent = "Passwords do not match";
+        confirmMsg.style.color = "rgb(230, 33, 79)";
+        pw2.style.backgroundColor = "rgb(245, 130, 157)";
+        pw2.style.borderColor = "rgb(230, 33, 79)";
+        passwordsMatch = false;
+        console.log(passwordsMatch);
+}
